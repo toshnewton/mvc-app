668f37fc9d5b90a32a8e7a9c87bcbcbec9c40b8f
Binita Singh <46681260+tanibi@users.noreply.github.com>
2019-11-04 17:24:54 -0600

    added router paths

--- routes/router.js
+++ routes/router.js
@@ -25,6 +25,10 @@ router.get('/index', (req, res, next) => {
 
 // Defer path requests to a particular controller
 router.use('/dev', require('../controllers/developer.js'))
+router.use('/instructor', require('../controllers/instructor.js'))
+router.use('/course', require('../controllers/course.js'))
+router.use('/section', require('../controllers/section.js'))
+router.use('/student', require('../controllers/student.js'))
 
 console.log('END routing')
 module.exports = router
