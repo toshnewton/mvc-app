60d7a03be999d79469cf348eb93d795db598c48a
Binita Singh <46681260+tanibi@users.noreply.github.com>
2019-11-04 17:42:42 -0600

    modified models/section.js

---
+++ models/section.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+const SectionSchema = new mongoose.Schema({
+
+  _id: {
+    type: Number,
+    required: true
+  },
+  sectionnumber: {
+    type: char,
+    required: true
+  },
+  days: {
+    type: String,
+    minlength: 3,
+    maxlength: 100,
+    required: true,
+  },
+  starttime: {
+    type: Number,
+    minlength: 3,
+    maxlength: 100,
+    required: true,
+  },
+  roomnumber: {
+    type: String,
+    minlength: 5,
+    maxlength: 100,
+    required: true,
+  },
+  instructorid: {
+    type: Number,
+    required: true
+  },
+  courseid: {
+    type: Number,
+    required: true
+  }
+
+})
+module.exports = mongoose.model('Instructor', SectionSchema)
