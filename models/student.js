e1c3f3706b76f7e48eb7aba443e1c70562f85cbf
toshnewton <36524417+toshnewton@users.noreply.github.com>
2019-11-05 13:38:01 -0600

    Update student.js

---
+++ models/student.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose')
+
+const StudentSchema = new mongoose.Schema({
+
+  _id: {
+    type: Number,
+    required: true
+  },
+  given: {
+    type: String,
+    minlength: 3,
+    maxlength: 100,
+    required: false,
+    default: 'Given name'
+  },
+  family: {
+    type: String,
+    minlength: 3,
+    maxlength: 100,
+    required: true,
+    default: 'Family name'
+  },
+  email: {
+    type: String,
+    minlength: 5,
+    maxlength: 100,
+    required: true,
+    unique: true
+  },
+  gpa: {
+    type: Number,
+    minlength: 1,
+    maxlength: 3,
+    required: true
+  },
+  github: {
+    type: String,
+    minlength: 4,
+    maxlength: 100,
+    required: true,
+    default: 'http://www.github.com/your-github'
+  },
+  website: {
+    type: String,
+    minlength: 4,
+    maxlength: 100,
+    required: true,
+    default: 'http://yourwebsite.com'
+  },
+  sectionID: {
+    type: Number,
+    minlength: 1,
+    maxlength: 4,
+    required: true
+  }
+
+})
+module.exports = mongoose.model('Student', SectionSchema)
