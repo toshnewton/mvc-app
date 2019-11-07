62ec83f6822ee37a7b63931fbdd62fe8f4f80871
aawajjoshi <s521315@nwmissouri.edu>
2019-11-07 13:36:56 -0600

    Seeded the sample Course data

--- controllers/course.js
+++ controllers/course.js
@@ -1,1 +1,34 @@
+const express = require('express')
+const api = express.Router()
+// const Model = require('../models/developer.js')
+const find = require('lodash.find')
+const notfoundstring = 'Could not find developer with id='
 
+// RESPOND WITH JSON DATA  --------------------------------------------
+
+// GET all JSON
+api.get('/findall', (req, res) => {
+  res.setHeader('Content-Type', 'application/json')
+  const data = req.app.locals.course.query
+  res.send(JSON.stringify(data))
+})
+
+// GET one JSON by ID
+api.get('/findone/:id', (req, res) => {
+  res.setHeader('Content-Type', 'application/json')
+  const id = parseInt(req.params.id)
+  const data = req.app.locals.course.query
+  const item = find(data, { _id: id })
+  if (!item) { return res.end(notfoundstring + id) }
+  res.send(JSON.stringify(item))
+})
+
+// RESPOND WITH VIEWS  --------------------------------------------
+
+// later
+
+// RESPOND WITH DATA MODIFICATIONS  -------------------------------
+
+// later
+
+module.exports = api
--- utils/seeder.js
+++ utils/seeder.js
@@ -1,5 +1,6 @@
 const Datastore = require('nedb') // set up a temporary (in memory) database
 const developerData = require('../data/developers.json') // read in data file
+const courseData = require('../data/course.json')
 
 // inject Express app to configure it - EVERYTHING in through argument list
 
@@ -10,12 +11,19 @@ module.exports = (app) => {
   db.developers = new Datastore() // new object property
   db.developers.loadDatabase() // call the loadDatabase method
 
+  db.course = new Datastore()
+  db.course.loadDatabase()
+
   // insert the sample data into our datastore
   db.developers.insert(developerData)
+  db.course.insert(courseData)
 
   // initialize app.locals (these objects are available to the controllers)
   app.locals.developers = db.developers.find(developerData)
+  app.locals.course = db.course.find(courseData)
+
   console.log(`${app.locals.developers.query.length} developers seeded`)
+  console.log(`${app.locals.course.query.length} courses seeded`)
 
   console.log('END Data Seeder. Sample data read and verified.')
 }
